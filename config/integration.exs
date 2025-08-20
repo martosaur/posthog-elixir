57f3d135dc09b0de18b347bd8520ccb6535e9ce8
martosaur <martosaur@gmail.com>
2025-08-19 20:56:48 -0700

    git delete config/integrations.exs

--- config/integration.exs
+++
@@ -1,8 +0,0 @@
-import Config
-
-config :posthog, :integration_config,
-  public_url: "https://us.i.posthog.com",
-  api_key: "phc_tEQ8UGRHdPBaT42yocd714aoiW4mBkmx6nTNrxjqLpf",
-  metadata: [:extra],
-  capture_level: :info,
-  in_app_otp_apps: [:logger_handler_kit]
